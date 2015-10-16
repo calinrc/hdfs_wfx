39c89d5113878cbbf59947cf58620963a6c9b021
calinrc <calin_cocan@yahoo.com>
2015-10-16 15:25:47 +0300

    update

--- java/hdfs_wfx_java/src/main/java/org/cgc/wfx/impl/FileSystemProxy.java
+++ java/hdfs_wfx_java/src/main/java/org/cgc/wfx/impl/FileSystemProxy.java
@@ -1,12 +1,12 @@
 /*
- *    FileSystemProxy.javafile written and maintained by Calin Cocan
+ *    FileSystemProxy.java file written and maintained by Calin Cocan
  *    Created on: Oct 05, 2015
  *
- * This work is free: you can redistribute it and/or modify it under the terms of GPL License Version 2.0
+ * This work is free: you can redistribute it and/or modify it under the terms of Apache License Version 2.0
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the License for more details.
- * You should have received a copy of the License along with this program. If not, see <http://choosealicense.com/licenses/gpl-2.0/>.
+ * You should have received a copy of the License along with this program. If not, see <http://choosealicense.com/licenses/apache-2.0/>.
 
  ********************************************************************************************************************* */
 
@@ -211,7 +211,6 @@ public class FileSystemProxy implements WfxPair {
 		InputStream in = null;
 		OutputStream out = null;
 		try {
-			log.info("Use deplate method for put file");
 			File file = new File(localPath);
 			in = new FileInputStream(file);
 			FileUpdateMonitor fileMonitor = new FileUpdateMonitor(progress, file.length());
